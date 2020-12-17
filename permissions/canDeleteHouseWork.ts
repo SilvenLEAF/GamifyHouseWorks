aa054f569119964007dd2069379c3fec53a188f9
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-17 07:51:49 +0530

    TypeScript with Express and React

---
+++ permissions/canDeleteHouseWork.ts
@@ -0,0 +1,19 @@
+import HouseWorkInterface from '../interfaces/databaseInterfaces/HouseWorkInterface';
+import UserInterface from '../interfaces/databaseInterfaces/UserInterface';
+import HouseWork from '../models/HouseWork';
+
+
+
+
+
+export default async (houseWorkId: any, user: UserInterface)=>{
+
+  const houseWork = await HouseWork.findById(houseWorkId) as HouseWorkInterface;
+
+  // user can only delete their own housework
+  if(houseWork.ownerId == user._id) return true;
+  
+  
+  //otherwise they can not
+  return false;
+}
